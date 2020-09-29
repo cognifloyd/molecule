2dce1a3a9814da9b97544b2547c6f547295d1139
Jacob Floyd <cognifloyd@gmail.com>
2020-09-29 11:38:51 -0500

    Add log folding when running on travis-ci
    Uses a function wrapper around execute_subcommand

--- molecule/command/base.py
+++ molecule/command/base.py
@@ -20,9 +20,11 @@
 
 import abc
 import collections
+import functools
 import glob
 import os
 
+import colorama
 import six
 
 import molecule.command
@@ -116,6 +118,33 @@ def execute_cmdline_scenarios(scenario_name, args, command_args, ansible_args=()
                 raise
 
 
+def _output_for_ci(func):
+    is_travis = os.getenv("TRAVIS") and os.getenv("CI")
+    if not is_travis:
+        return func
+
+    @functools.wraps(func)
+    def travis_wrapper(config, subcommand):
+        print(
+            (
+                "travis_fold:start:{0}.{1}"
+                "{2}Molecule --> Scenario: '{0}' --> Action: '{1}'{3}"
+            ).format(
+                config.scenario.name,
+                subcommand,
+                colorama.Style.BRIGHT + colorama.Fore.CYAN,
+                colorama.Style.RESET_ALL,
+            )
+        )
+        try:
+            func(config, subcommand)
+        finally:
+            print("travis_fold:end:{}.{}".format(config.scenario.name, subcommand))
+
+    return travis_wrapper
+
+
+@_output_for_ci
 def execute_subcommand(config, subcommand):
     command_module = getattr(molecule.command, subcommand)
     command = getattr(command_module, util.camelize(subcommand))
